a49f88ef286faf109596af97990cecb3ed3120b4
Daniel Pozzi <bonndan76@googlemail.com>
2013-04-10 21:46:50 +0200

    abstract writer implements config injection

--- MdMan/Writer/Abstract.php
+++ MdMan/Writer/Abstract.php
@@ -16,18 +16,48 @@ abstract class MdMan_Writer_Abstract implements MdMan_Writer
 {
     /**
      * the markdown tree
-     * @var \MdMan_MarkdownTree 
+     * @var MdMan_MarkdownTree 
      */
     protected $tree;
+    
+    /**
+     * the plugin configuration
+     * @var MdMan_Configuration $config
+     */
+    protected $config;
     
     /**
      * Inject the markdown into the writer.
      * 
      * @param \MdMan_MarkdownTree $tree
      */
-    public function setMarkdownTree(\MdMan_MarkdownTree $tree)
+    public function setMarkdownTree(MdMan_MarkdownTree $tree)
     {
         $this->tree = $tree;
+    }
+    
+    /**
+     * Inject the configuration into the writer.
+     * 
+     * @param MdMan_Configuration $config
+     */
+    public function setConfig(MdMan_Configuration $config)
+    {
+        $this->config = $config;
+    }
+    
+    /**
+     * Returns the path of the output target.
+     * 
+     * @return string
+     */
+    protected function getOutputTarget()
+    {
+        $outDir  = $this->getOption(MdMan_Configuration::OUTDIR_OPTION);
+        $outFile = $this->getOption(MdMan_Configuration::OUTFILE_OPTION);
+        $target = $outDir . DIRECTORY_SEPARATOR . $outFile;
+        
+        return $target;
     }
     
     /**
@@ -35,5 +65,5 @@ abstract class MdMan_Writer_Abstract implements MdMan_Writer
      * 
      * @param MdMan_Configuration $config
      */
-    abstract public function execute(MdMan_Configuration $config);
+    abstract public function execute();
 }
