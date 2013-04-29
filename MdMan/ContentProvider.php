509f693b426815cb65b2d21eafb71125e122aa0b
Daniel Pozzi <bonndan76@googlemail.com>
2013-04-29 21:38:25 +0200

    changed the "tree" interface name to content provider

---
+++ MdMan/ContentProvider.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * MdMan_ContentProvider
+ * 
+ * @package MdMan
+ * @author  Daniel Pozzi <bonndan76@googlemail.com>
+ */
+
+/**
+ * Interface expected by writers which work on the markdown tree.
+ * 
+ * @package MdMan
+ * @author  Daniel Pozzi <bonndan76@googlemail.com>
+ */
+interface MdMan_ContentProvider
+{
+    /**
+     * Returns an associative array of packages and the classes' markdown
+     * 
+     * @return array (package => class => markdown)
+     */
+    public function getTree();
+    
+    /**
+     * Instructs the content provider to collect certain blocks.
+     * 
+     * @param string $tagName
+     */
+    public function collectTagsOfType($tagName);
+    
+    /**
+     * Returns the blocks of a certain type.
+     * 
+     * @param string $tagName
+     * @return array
+     */
+    public function getTagsOfType($tagName);
+}
